b2429ed8d68d3a3e679c89149cd013cac4dd6063
Eduardo Pineda <edibozu@gmail.com>
2020-09-30 18:20:53 -0400

    cleaned up app.js file and wrote a readme file specifically for this application. An html file was created within the ouput folder when the application was complete. images were added inside the readme to provide a visual. Those images were store in the Assets folder

--- app.js
+++ app.js
@@ -45,12 +45,11 @@ function initialQuestions(userInput) {
     })
     .catch(function (err) {
       if (err) throw err;
-      console.log("Logged initial prompt inputs!");
-    });
+      });
 }
 
 function positionQuestions(userResponse) {
-  if (positionInputs.employeeRole === "Manager") {
+  if (userResponse.role === "Manager") {
     inquirer
       .prompt([
         {
@@ -64,7 +63,7 @@ function positionQuestions(userResponse) {
           userResponse.name,
           userResponse.id,
           userResponse.email,
-          userResponse.officeNumber
+          positionResponse.officeNumber
         );
         employeeArray.push(manager);
         stopPrompt();
@@ -72,7 +71,7 @@ function positionQuestions(userResponse) {
       .catch(function (err) {
         if (err) throw err;
       });
-  } else if (userResponse.employeeRole === "Engineer") {
+  } else if (userResponse.role === "Engineer") {
     inquirer
       .prompt([
         {
@@ -86,35 +85,57 @@ function positionQuestions(userResponse) {
           userResponse.name,
           userResponse.id,
           userResponse.email,
-          userResponse.github
+          positionResponse.github
         );
         employeeArray.push(engineer);
-        stropPrompt();
-  }).catch(function(err){
-      if(err) throw err
-
-  })
+        stopPrompt();
+      })
+      .catch(function (err) {
+        if (err) throw err;
+      });
+  } else {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "school",
+          message: "Where did employee attend school? ",
+        },
+      ])
+      .then(function (positionResponse) {
+        const intern = new Intern(
+          userResponse.name,
+          userResponse.id,
+          userResponse.email,
+          positionResponse.school
+        );
+        employeeArray.push(intern);
+        stopPrompt();
+      })
+      .catch(function (err) {
+        if (err) throw err;
+      });
+  }
 }
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function stopPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "stop",
+        message: "Would you like to stop adding employees?",
+      },
+    ])
+    .then(function (response) {
+      if (response.stop) {
+        const employeeList = render(employeeArray);
+        fs.writeFile(outputPath, employeeList, function (err) {
+            console.log("Logged initial prompt inputs!");
+          if (err) throw err;
+        });
+      } else {
+        initialQuestions();
+      }
+    });
+}
