7277a0cc8a8ff59768ae605548717075d53e3c79
Eduardo Pineda <edibozu@gmail.com>
2020-09-30 18:44:09 -0400

    added comments to app.js the explain what the groups/lines of code are doing

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Devs and files that will be used of pulled upon this this application
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -9,9 +10,9 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+// created an empty array, where the data will go once the user answers all of the prompts.
 const employeeArray = [];
-
+// The initial questions that will be asked about every employee.
 initialQuestions();
 
 function initialQuestions(userInput) {
@@ -47,7 +48,9 @@ function initialQuestions(userInput) {
       if (err) throw err;
       });
 }
-
+// Created a second set of prompts because every employee wil have different roles.
+// Depending the the role selected during the prompts, this will create a specific qustion to be asked.
+// A manager is aked to proved an office number.
 function positionQuestions(userResponse) {
   if (userResponse.role === "Manager") {
     inquirer
@@ -66,11 +69,13 @@ function positionQuestions(userResponse) {
           positionResponse.officeNumber
         );
         employeeArray.push(manager);
+        // After every employee input, the user will be asked whether they'd like to stop adding employees.
         stopPrompt();
       })
       .catch(function (err) {
         if (err) throw err;
       });
+      // An engineer will be asked to provide a GitHub account.
   } else if (userResponse.role === "Engineer") {
     inquirer
       .prompt([
@@ -94,6 +99,7 @@ function positionQuestions(userResponse) {
         if (err) throw err;
       });
   } else {
+      // an intern will be asked to provide the name of the school they attended. 
     inquirer
       .prompt([
         {
@@ -117,7 +123,9 @@ function positionQuestions(userResponse) {
       });
   }
 }
-
+// This function is what asks the user whether they's like to stop adding employees.
+// If they select no, the initial qiestions will be asked again.
+// This will continue until the user answers with "no".
 function stopPrompt() {
   inquirer
     .prompt([
@@ -130,6 +138,7 @@ function stopPrompt() {
     .then(function (response) {
       if (response.stop) {
         const employeeList = render(employeeArray);
+        // When the user is done logging in employees, the application will create an html file within the "ouput" folder.
         fs.writeFile(outputPath, employeeList, function (err) {
             console.log("Logged initial prompt inputs!");
           if (err) throw err;
